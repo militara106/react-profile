b5a250cb50c337da1826fb0ef4e62b2a5a607a6d
Alex <militara106@gmail.com>
2020-02-01 18:00:10 -0800

    font resizing fix

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -7,22 +7,25 @@ import Scrollspy from "react-scrollspy";
 function CustomNavbar(props) {
   return (
     <div>
-    <Navbar className="justify-content-center" bg="dark" variant="dark">
-      <Nav defaultActiveKey={window.location.pathname}>
-        {/* Scroll Spy Links*/}
-        <Scrollspy items={["section-1", "section-2", "section-3"]} currentClassName="active">
-          <Nav.Item className="verticalText">
-            <Nav.Link href="#section-1">About</Nav.Link>
-          </Nav.Item>
-          <Nav.Item className="verticalText">
-            <Nav.Link href="#section-2">Portfolio</Nav.Link>
-          </Nav.Item>
-          <Nav.Item className="verticalText">
-            <Nav.Link href="#section-3">Contact</Nav.Link>
-          </Nav.Item>
-        </Scrollspy>
-      </Nav>
-    </Navbar>
+      <Navbar className="justify-content-center" bg="dark" variant="dark">
+        <Nav defaultActiveKey={window.location.pathname}>
+          {/* Scroll Spy Links*/}
+          <Scrollspy
+            items={["section-1", "section-2", "section-3"]}
+            currentClassName="active"
+          >
+            <Nav.Item className="verticalText">
+              <Nav.Link href="#section-1">About</Nav.Link>
+            </Nav.Item>
+            <Nav.Item className="verticalText">
+              <Nav.Link href="#section-2">Portfolio</Nav.Link>
+            </Nav.Item>
+            <Nav.Item className="verticalText">
+              <Nav.Link href="#section-3">Contact</Nav.Link>
+            </Nav.Item>
+          </Scrollspy>
+        </Nav>
+      </Navbar>
     </div>
   );
 }
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,9 +1,13 @@
 import React from "react";
 import BorderContainer from "../components/BorderContainer";
+import Image from "react-bootstrap/Image";
 
 function Home() {
     return(
-        <BorderContainer>Home</BorderContainer>
+        <BorderContainer>
+            <Image src="SecondCup.jpg" className="homeImage" thumbnail/>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Facilisi nullam vehicula ipsum a arcu cursus vitae.
+        </BorderContainer>
     )
 }
 
